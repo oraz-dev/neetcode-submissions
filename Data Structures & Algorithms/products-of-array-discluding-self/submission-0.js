168b9db341f3b60e62b7f8a462bb5caf8ab1f2ed
oraz-dev <165394503+oraz-dev@users.noreply.github.com>
2026-08-13 23:11:07 +0500

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @return {number[]}
+   */
+  productExceptSelf(nums) {
+    const n = nums.length
+    const output = new Array(n).fill(1)
+
+    for (let i = 1;i < n; i++) {
+      output[i] = output[i - 1] * nums[i - 1]
+    }
+
+    let postfix = 1
+    for (let i = n - 1; i >= 0; i--) {
+      output[i] = output[i] * postfix
+      postfix = nums[i] * postfix
+    }
+
+    return output
+  }
+}
